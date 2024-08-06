360424e0bf8e3f6ca4b9f6979ef14ff868149f0f
Mychaela Tran <tranmychaela@gmail.com>
2024-08-06 16:55:52 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
-const data = document.querySelector('.data')
-const googleSignInBtn = document.querySelector('.g-signin2')
+
 
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
     $("#name").text(profile.getName()); /*the name id on our html gets the data of name from this function*/
     $("#email").text(profile.getEmail());
     $("#image").attr('src', profile.getImageUrl());
-    data.style.display = 'block';
-    googleSignInBtn.style.display = 'none';
-
+    $(".data").css("display","block");
+    $(".g-signin2").css("display","none");
     
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
     
@@ -19,8 +17,8 @@ var auth2 = gapi.auth2.getAuthInstance();
 auth2.signOut().then(function () {
     console.log('User signed out.');
     alert("You have been signed out successfully!")
-    googleSignInBtn.style.display = 'block'
-    data.style.display = 'none';
+    $(".g-signin2").css("display","block");
+    $(".data").css("display","none");
 
 });
 }
